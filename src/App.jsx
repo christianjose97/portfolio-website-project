497e02ce1f38317224adb55909ed3af1cbc811b1
Christian Jose <christian.jose443@gmail.com>
2024-02-23 23:11:43 -0500

    added hero section & contact button and headshot

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+// App.jsx
+
+import React from 'react';
 import styles from './App.module.css';
 import { Navbar } from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
 
 function App() {
-  return <div className={styles.App}> 
-    <Navbar />
-  </div>;
+  return (
+    <div className={styles.App}>
+      <Navbar />
+      <Hero />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+// Hero.jsx
+
+import React from 'react';
+import { getImageUrl } from '../../utils';
+import styles from './Hero.module.css';
+
+const Hero = () => {
+  return (
+    <section className={styles.hero}>
+      <div className={styles.heroContent}>
+        <h1>Hi, I'm Chris</h1>
+        <p>I'm a creative backend developer</p>
+        <a href="mailto:myemail@email.com">Contact me</a>
+      </div>
+      <img src={getImageUrl("hero/heroImage.jpg")} alt="Hero image of me" className={styles.heroImage} />
+    </section>
+  );
+};
+
+export default Hero;
